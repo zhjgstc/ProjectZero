bf86b7935abff1adc8f7091dd5e7940f060e0061
章建国 <zhang.jianguo@hotmail.com>
2020-07-18 19:19:33 +0800

    增加tab页对应的数据弹出

--- src/page/main/Content.tsx
+++ src/page/main/Content.tsx
@@ -4,6 +4,8 @@ import MySqlEvent from '../../component/content/MySql/Event/Event';
 import MySqlFunction from '../../component/content/MySql/Function/Function';
 import MySqlTable from '../../component/content/MySql/Table/Table';
 import MySqlView from '../../component/content/MySql/View/View';
+import MySqlPageTable from '../MySql/Table/Table';
+
 import * as Utils from '../../utils/Utils';
 import { Chip, Icon, Tooltip } from "@material-ui/core";
 
@@ -19,10 +21,6 @@ interface IProps {
 interface IState {
     connected: boolean,
     chipList: Array<chipItem>,
-    selectItem: {
-        title: string,
-        item?: chipItem
-    }
 }
 
 interface chipItem {
@@ -30,7 +28,9 @@ interface chipItem {
     name: string
     host: MySqlModels.IHostItem,
     database: MySqlModels.IDatabase,
-    action: string
+    action: string,
+    component: any,
+    selected: boolean
 }
 
 export default class Content extends React.Component<IProps, IState> {
@@ -39,27 +39,34 @@ export default class Content extends React.Component<IProps, IState> {
         this.state = {
             connected: false,
             chipList: new Array<chipItem>(),
-            selectItem: {
-                title: "对象"
-            }
         }
+
     }
 
     componentDidMount() {
-
+        var list = this.state.chipList;
+        list.push({
+            title: "对象",
+            name: "",
+            host: this.props.item.host,
+            database: this.props.item.database,
+            action: this.props.item.action,
+            component: this.getMainComponent(),
+            selected: true
+        });
+        this.setState({ chipList: list });
     }
 
     callBackComponent = (host: MySqlModels.IHostItem, database: MySqlModels.IDatabase, action: string, name: string) => {
         var list = this.state.chipList;
-        var selectItem = this.state.selectItem;
         var title = name + "@" + database.name + "(" + host.item.name + ")";
         var flag = false;
         for (let index = 0; index < list.length; index++) {
             const element = list[index];
             if (host.item.id === element.host.item.id && element.title === title) {
                 flag = true;
-                selectItem.title = title;
             } else {
+                list[index].selected = false;
             }
         }
 
@@ -70,44 +77,57 @@ export default class Content extends React.Component<IProps, IState> {
                 name: name,
                 host: host,
                 database: database,
-                action: action
+                action: action,
+                component: this.getComponent(host, action),
+                selected: true
             };
             list.push(item);
-            selectItem.title = title;
         }
-        this.setState({ chipList: list, selectItem: selectItem });
+        this.setState({ chipList: list });
+    }
+
+    getComponent = (host: MySqlModels.IHostItem, action) => {
+        if (action === "表") {
+            return <MySqlPageTable></MySqlPageTable>
+        } else {
+            return null;
+        }
+    }
+
+    setSelectItemOnClick = (index: number) => {
+        for (let i = 0; i < this.state.chipList.length; i++) {
+            this.state.chipList[i].selected = false;
+        }
+        this.state.chipList[index].selected = true;
+        this.setState({ chipList: this.state.chipList });
+    }
+
+    deleteChipItem = (index: number) => {
+        this.state.chipList.splice(index, 1);
+        this.state.chipList[0].selected = true;
+        this.setState({ chipList: this.state.chipList });
     }
 
     renderChips = () => {
         return this.state.chipList.map((item: chipItem, index: number) => {
             return (
+
                 <Tooltip title={item.title} key={index}>
                     <Chip
-                        variant={this.state.selectItem.title === item.title ? "default" : "outlined"}
+                        variant={item.selected ? "default" : "outlined"}
                         color="primary"
                         style={{ width: "180px", borderRadius: "0px" }}
                         size="medium"
                         label={item.title}
-                        onClick={() => {
-                            var selectItem = this.state.selectItem;
-                            selectItem.title = item.title;
-                            this.setState({ selectItem: selectItem });
-                        }}
-                        onDelete={() => {
-                            var list = this.state.chipList;
-                            list.splice(index, 1);
-                            var selectItem = this.state.selectItem;
-                            selectItem.title = "对象";
-                            selectItem.item = undefined;
-                            this.setState({ selectItem: selectItem, chipList: list });
-                        }}
+                        onClick={() => this.setSelectItemOnClick(index)}
+                        onDelete={item.title === "对象" ? null : () => this.deleteChipItem(index)}
                     />
                 </Tooltip>
             )
         })
     }
 
-    renderContent = () => {
+    getMainComponent = () => {
         switch (this.props.item.action) {
             case "表":
                 {
@@ -133,26 +153,19 @@ export default class Content extends React.Component<IProps, IState> {
                         <MySqlEvent></MySqlEvent>
                     )
                 }
+        }
+    }
 
+    renderContent = () => {
+        var index = Utils.Loadsh.findIndex(this.state.chipList, { selected: true });
+        if (this.state.chipList[index] && this.state.chipList[index].component) {
+            return this.state.chipList[index].component;
         }
     }
 
     render() {
         return (
             <div>
-                <Chip
-                    variant={this.state.selectItem.title === "对象" ? "default" : "outlined"}
-                    color="primary"
-                    style={{ width: "180px", borderRadius: "0px" }}
-                    size="medium"
-                    label="对象"
-                    onClick={() => {
-                        var selectItem = this.state.selectItem;
-                        selectItem.title = "对象";
-                        selectItem.item = undefined;
-                        this.setState({ selectItem: selectItem });
-                    }}
-                />
                 {
                     this.renderChips()
                 }
@@ -160,7 +173,6 @@ export default class Content extends React.Component<IProps, IState> {
                 {
                     this.renderContent()
                 }
-
             </div>
         )
     }
